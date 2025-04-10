f73f6b672e90a42dd16fed147348e0a7430c20ac
july yukari <julyukari.cdd@gmail.com>
2025-04-10 08:53:05 -0300

    auteracao exercicio2

--- exercicio2.py
+++ exercicio2.py
@@ -1,2 +1,5 @@
+#start= De onde vai começar
+#stop = aonde vai para sempre adicionando mas 1 , pois o ultimo numero ele sempre subtrai
+#step = como vai ser a contagem, se for do menor para o maior sempre (-1, negativo), do maior para o menor (positivo)
 for x in range (11,0,-1):
     print(x ,end=" ")
